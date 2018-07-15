2b0bad38e769a391ae68190189538a125d06e500
Ollie Pai <ollie.pai2003@gmail.com>
2018-07-15 14:25:50 -0700

    Worked on parsing requests

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -34,36 +36,77 @@ class ThreadedSocket extends Thread {
             line = in.readLine();
             String request_method = line;
             System.out.println(line);
+            String contents = parseRequest(line);
             line = "";
-            int postDataI = -1;
-            while ((line = in.readLine()) != null && (line.length() != 0)) {
-                System.out.println(line);
-                if (line.indexOf("Content-Length:") > -1) {
-                    postDataI = new Integer(
-                            line.substring(
-                                    line.indexOf("Content-Length:") + 16,
-                                    line.length())).intValue();
-                }
-            }
-            String postData = "";
-  
-            if (postDataI > 0) {
-                char[] charArray = new char[postDataI];
-                in.read(charArray, 0, postDataI);
-                postData = new String(charArray);
-            }
-          
+            
+//            int postDataI = -1;
+//            while ((line = in.readLine()) != null && (line.length() != 0)) {
+//                System.out.println(line);
+//                if (line.indexOf("Content-Length:") > -1) {
+//                    postDataI = new Integer(
+//                            line.substring(
+//                                    line.indexOf("Content-Length:") + 16,
+//                                    line.length())).intValue();
+//                }
+//            }
+//            String postData = "";
+//  
+//            if (postDataI > 0) {
+//                char[] charArray = new char[postDataI];
+//                in.read(charArray, 0, postDataI);
+//                postData = new String(charArray);
+//            }
+            
+            
             out.println("HTTP/1.0 200 OK");
             out.println("Content-Type: text/html; charset=utf-8");
             out.println("Server: MINISERVER");
             out.println("");
-            out.println("<h1>Welcome to the Web Site</h1>");
-            out.println("Username: <input></input><br>Password: <input type='password'></input>");
-            out.println("<button>submit</button>");
+            out.println("<script> function load() {document.location = \"https://google.com\"} </script>");
+            out.println("<button onclick='load()'>submit</button>");
+            contents = "";
             out.close();
             insocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    public String parseRequest(String request) {
+    		BufferedReader br;
+		String contents = "";
+    		if (request.equals("GET /file1 HTTP/1.1")) {
+			try {
+				br = new BufferedReader(new FileReader("file1.html"));
+	            String line = br.readLine();
+	            while (line != null) {
+	    	            	contents += line;
+	    	            	line = br.readLine();
+	                }
+			} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			}
+    		}
+    		else if (request.equals("GET /file2 HTTP/1.1")) {
+    			try {
+					br = new BufferedReader(new FileReader("file2.html"));
+	                String line = br.readLine();
+	                while (line != null) {
+	    	            	contents += line;
+	    	            	line = br.readLine();
+	                }
+			} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			}
+    		}
+    		return contents;
     }
 }
